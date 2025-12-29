d621e1fa868be034b3d31c7a2bf68788957c55eb
Nicola Baldassar <nicola@MACBOOKAIR-B3CB.station>
2025-12-29 19:21:51 +0100

    prime regole del gioco e prima interfaccia grafica basilare

---
+++ gui.py
@@ -0,0 +1,110 @@
+import tkinter as tk
+from rules import Nonogram
+
+# dimensione in pixel di ogni cella
+CELL_SIZE = 40
+
+# centra la finestra e aggiungici dello spazio tra i bordi della finestra e la griglia
+def center_window(win):
+    win.update_idletasks()
+
+    width = win.winfo_width()
+    height = win.winfo_height()
+
+    screen_w = win.winfo_screenwidth()
+    screen_h = win.winfo_screenheight()
+
+    x = (screen_w // 2) - (width // 2)
+    y = (screen_h // 2) - (height // 2)
+    win.geometry(f"{width}x{height}+{x}+{y}")
+
+class NonogramGUI:
+    def __init__(self, master, model: Nonogram):
+        self.master = master
+        self.model = model
+
+        # titolo finestra
+        master.title("Nonogram")
+
+        self.margin = 40
+
+        # canvas dove si disegna la griglia
+        self.canvas = tk.Canvas(
+            master,
+            width = model.cols * CELL_SIZE + self.margin * 2,
+            height = model.rows * CELL_SIZE + self.margin * 2,
+            bg = "white"
+        )
+        self.canvas.pack()
+
+        # disegna la griglia
+        self.draw_grid()
+
+        # aggiunge l'evento click sulle celle
+        self.canvas.bind("<Button-1>", self.on_click)
+
+    # disegna la griglia e contenuto delle celle
+    def draw_grid(self):
+        self.canvas.delete("all")
+
+        for r in range(self.model.rows):
+            for c in range(self.model.cols):
+                x1 = (c * CELL_SIZE) + self.margin
+                y1 = (r * CELL_SIZE) + self.margin
+                x2 = x1 + CELL_SIZE
+                y2 = y1 + CELL_SIZE
+
+                value = self.model.get_cell(r, c)
+
+                # imposta lo sfondo della cella in base allo stato
+                if value == 1:
+                    color = "black"
+                elif value == 0:
+                    color = "lightgray"
+                else:
+                    color = "white"
+                
+                self.canvas.create_rectangle(
+                    x1, y1, x2, y2, 
+                    fill = color,
+                    outline = "gray"
+                )
+
+    # gestione click del mouse
+    def on_click(self, event):
+        c = (event.x - self.margin) // CELL_SIZE
+        r = (event.y - self.margin) // CELL_SIZE
+
+        if r < 0 or c < 0 or r >= self.model.rows or c >= self.model.cols:
+            return
+        
+        current = self.model.get_cell(r, c)
+
+        # imposta il ciclo di click tale che: None -> Pieno -> Vuoto -> None
+        if current is None:
+            new = 1
+        elif current == 1:
+            new = 0
+        else:
+            new = None
+        
+        self.model.set_cell(r, c, new)
+
+        # ridisegna la cella
+        self.draw_grid()
+
+
+
+
+if __name__ == "__main__":
+    model = Nonogram(
+        8, 8,
+        row_hints=[[1], 3, [1,1], [5], [2], [1], 3, [1,1]],
+        col_hints=[[2], [2], [1, 1], [3], [1], [1], 3, [1,1]]
+    )
+    root = tk.Tk()
+    gui = NonogramGUI(root, model)
+    
+    center_window(root)
+
+    root.mainloop()
---
+++ rules.py
@@ -0,0 +1,69 @@
+class Nonogram:
+    def __init__(self, rows: int, cols: int, row_hints: list, col_hints: list):
+        # settare la dimensione della griglia
+        self.rows = rows
+        self.cols = cols
+
+        # valori griglia:
+        # None = non deciso
+        # 1 = cella piena
+        # 0 = cella vuota
+        self.grid = [[None for _ in range(cols)] for _ in range(rows)]
+
+        # indizi: lista di liste
+        self.row_hints = row_hints
+        self.col_hints = col_hints
+    
+    # funzione per impostare il valore di una cella
+    def set_cell(self, r: int, c: int, value: int) -> None:
+        if value not in (0, 1, None):
+            raise ValueError("Valore cella non valido.")
+        self.grid[r][c] = value
+
+    # restituisce il valore della cella
+    def get_cell(self, r: int, c: int):
+        return self.grid[r][c]
+    
+    # dati dei vettori di 0 e 1 restituisce le sequenze di 1 consecutivi
+    def extract_groups(self, line:list) -> list:
+        if None in line:
+            return None
+        groups = []
+        count = 0
+
+        for cell in line:
+            if cell == 1:
+                count += 1
+            elif count > 0:
+                groups.append(count)
+                count = 0
+            
+        if count > 0:
+            groups.append(count)
+        
+        return groups
+
+    # verifica se la riga r rispetta gli indizi, ritorna True, False o None se ci sono delle celle None
+    def row_valid(self, r: int) -> bool | None:
+        extracted = self.extract_groups(self.grid[r])
+        if extracted is None:
+            return None
+        return extracted == self.row_hints[r]
+    
+    def col_valid(self, c: int) -> bool | None:
+        col = [self.grid[r][c] for r in range(self.rows)]
+        extracted = self.extract_groups(col)
+        if extracted is None:
+            return None
+        return extracted == self.col_hints(c)
+    
+    # controlla se tutte le righe e colonne rispetto gli indizi
+    def is_solved(self) -> bool:
+        for r in range(self.rows):
+            if self.row_valid(r) is not True:
+                return False
+        for c in range(self.cols):
+            if self.col_valid(c) is not True:
+                return False
+        return True
+    
